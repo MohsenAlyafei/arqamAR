fad5f638e38e65d2bd1bbe977de49181411b97bb
Mohsen Alyafei <51336583+MohsenAlyafei@users.noreply.github.com>
2020-07-28 19:38:09 +0300

    Add files via upload
    Function updated to an additional optional parameter for removing trailing zeros in fractions

--- arqamNumberSplit/arqamNumberSplit.js
+++ arqamNumberSplit/arqamNumberSplit.js
@@ -1,17 +1,21 @@
 /************************************************************************
-* @function : arqamNumberSplit(Num, DecimalPlaces)
+* @function : arqamNumberSplit(Num, DecimalPlaces, [RemovedTrailingZeros])
 * @purpose  : Splits a Float Number into its whole and fractional parts
 *             taking into account the required rounding for the
 *             Decimal Part.
 *             The Whole Part is not affected by the rounding
 *             of the Decimal Part.
-* @version  : 1.01
+* @version  : 1.02
 * @author   : Mohsen Alyafei
-* @date     : 15 July 2020
+* @date     : 25 July 2020
 * @param      {Num} Float Number to be splitted.
 * @param      {DecimalPlaces} Number of deciaml places for rounding
 *             0 : No fraction need. An "" empty string is returned
 *             -1: No Rounding required (keep original fraction as is)
+*             The max number of deciaml place is 20.
+* @param      {RemovedTrailingZeros} [Optional] removes trailing zeros from
+*             the fraction part irrespective of the number of required
+*             decimal places.
 * @return   : Arrays of 2 {String} Elements Containing the Whole Part
 *             and the Fraction Part as string.
 *             The Fraction is formatted to the decimal places requested.
@@ -20,7 +24,7 @@
 *             Note: Empty strings are handy for logical testing.
 *************************************************************************/
 
-export function arqamNumberSplit(Num,DecimalPlaces){
+export function arqamNumberSplit(Num,DecimalPlaces,RemovedTrailingZeros=0){
 Num= (Num+"").split(0.1.toLocaleString().substr(1,1));
 let a, WholePart=Num[0], fractionalPart= Num[1];
 if (DecimalPlaces>=0) {
@@ -28,6 +32,7 @@ fractionalPart = Num.length ==2 ?
        fractionalPart && (fractionalPart="0."+fractionalPart,
        a=(+fractionalPart).toFixed(DecimalPlaces),
        fractionalPart = ( a <1 ? a : fractionalPart).substr(2,DecimalPlaces)) : "";
+  if (RemovedTrailingZeros) fractionalPart=(""+(+("0."+fractionalPart))).substr(2);
 }
 return [!+WholePart? "" : WholePart, !+fractionalPart?"":fractionalPart];
 }

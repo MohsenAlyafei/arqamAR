cc13f3204859ca25a8615a4f3c30ae88c4a71286
Mohsen Alyafei <51336583+MohsenAlyafei@users.noreply.github.com>
2020-08-01 00:07:26 +0300

    Add files via upload

--- src/numberToWordsAr.js
+++ src/numberToWordsAr.js
@@ -1,7 +1,7 @@
 /*********************************************************************
 * @function    : numberToWordsAr(Number, [{options}])
 * @purpose     : Converts Numbers to Arabic Words
-* @version     : 1.10
+* @version     : 1.11
 * @author      : Mohsen Alyafei
 * @date        : 30 July 2020
 * @param       : {Number} [integer or number in string form]
@@ -23,14 +23,14 @@ const TableScalesSingle= ["","ألف","مليون","بليون","ترليون","
       TableScalesPlural= ["","آلاف","ملايين","بلايين","ترليونات","كوادرليونات","كوينتليونات","سكستليونات"],
       TableMale        = ["","واحد","اثنان","ثلاثة","أربعة","خمسة","ستة","سبعة","ثمانية","تسعة","عشرة"],
       TableFemale      = ["","واحدة","اثنتان","ثلاث","أربع","خمس","ست","سبع","ثمان","تسع","عشر"];
-// ----
+
 export function numberToWordsAr(NumIn=0, {Feminine="", Comma="", SplitHund="", Miah="", TextAfter=""}={}) {
 if (NumIn == 0) return "صفر";                          // if 0 or "0" then "zero"
 let NumberInWords="", IsLastEffTriplet=false,Triplet,digits,TableUnits,Table11_19,SpWa= " و"; // Init vars
 Miah = (Miah==="on") ? "مئة" : "مائة";                 // Select chosen Miah (Hundred) Option
 // ---- Pickup the Default Table (Masculine)
-[TableUnits,Table11_19,Table11_19[0]]=[ [...TableMale], [...TableMale], TableFemale[10] ],
-[Table11_19[1],Table11_19[2]] = ["أحد", "إثنا"];       // Masculine starting words for 11 and 12
+[TableUnits, Table11_19, Table11_19[0]] = [ [...TableMale], [...TableMale], TableFemale[10] ],
+[Table11_19[1], Table11_19[2]         ] = ["أحد" , "إثنا"];  // Masculine starting words for 11 and 12
 
 NumIn = "0".repeat((NumIn+="").length * 2 % 3) + NumIn; // Convert Number to a Triplets String
 let L = NumIn.length;
@@ -46,40 +46,37 @@ return NumberInWords; // All done
 //------------------------------------------------------------------
 //    Core Function Converts 1 Triplet (1 to 999) to Arabic Words
 //------------------------------------------------------------------
-function oneTripletToWords(Scale) {      // Core Function called for each Triplet
+function oneTripletToWords(Scale) {
 let Num_100  = ~~(Triplet/100),          // Hundreds (1 digit)
     Num_99   = Triplet % 100,            // 00 to 99
     Num_Unit = TableUnits[Num_99 % 10],  // 0 to 9 (string)
     Num_Tens = ~~(Num_99/10),            // Tens   (1 digit)
     Word_100 = "";                       // Holds words for Hundreds
-
-if (Feminine ==="on" && IsLastEffTriplet)  { // Use the Feminine tables only if Last Effective Triplet
-   [TableUnits,Table11_19,Table11_19[0]]=[ [...TableFemale], [...TableFemale], TableMale[10] ],
-   [Table11_19[1],Table11_19[2]]        = ["إحدى","إثنتا"]; // Feminine starting words for 11 and 12
-   if (Num_99 > 19) TableUnits[1] = "إحدى";
+// ---- If Feminine, use the Feminine tables only if Last Effective Triplet
+if (Feminine ==="on" && IsLastEffTriplet)  {
+   [TableUnits, Table11_19, Table11_19[0]] = [ [...TableFemale], [...TableFemale], TableMale[10] ],
+   [Table11_19[1], Table11_19[2]         ] = ["إحدى" , "إثنتا"]; // Feminine starting words for 11 and 12
+   if (Num_99 > 19) TableUnits[1]          = "إحدى";
    }
-// ----  Do Hundreds (100 to 900)
-if (Num_100) {
-   Word_100 = Num_100 > 2
-   ? TableFemale[Num_100] + (SplitHund === "on" ? " " : "") + Miah  // 300-900
-   : Num_100 === 1 ? Miah                                                // 100
-     : Miah.slice(0,-1) + (Scale && !Num_99 || TextAfter ?"تا":"تان");  // For 200 Use either مئتا or مئتان
-   }
-// ----  Do 0-99 and Join Join Hund, Tens, and Units
+// ---- Do Hundreds (100 to 900)
+Num_100 && (Word_100 = Num_100 > 2
+   ? TableFemale[Num_100] + (SplitHund === "on" ? " " : "") + Miah      // 300-900
+   : Num_100 === 1 ? Miah                                               // 100
+     : Miah.slice(0,-1) + (Scale && !Num_99 || TextAfter ?"تا":"تان")); // For 200 Use either مئتا or مئتان
+// ---- Do 0-99 and Join Join Hund, Tens, and Units
 let Words999 = Word_100 + (Num_100 && Num_99 ? SpWa : "") +
     (Num_99 > 19                                                   // 20-99
     ? Num_Unit + (Num_Unit ? SpWa : "") +                          // Units و and
       (Num_Tens === 2 ? "عشر" : TableFemale[Num_Tens]) + "ون"      // Add Woon for 20's or 30's to 90's
     : Num_99 > 10 ? Table11_19[Num_99-10] + " " + Table11_19[0] :  // 11-19
       TableUnits[Num_99]);
-
-if (Scale) {                                                  // If Scale Name then add Scale Name
+// ---- Add Scale Name if applicable
+if (Scale) {
    let Word_100Wa = (Num_100 ? Word_100 + SpWa :"") + Scale;  // Default Scale Name
    Words999 = Num_99 > 2
    ?  Words999 + (Num_99 < 11 ? " "+ TableScalesPlural[digits/3-1]: " "+Scale+"ًا") // For 3 to 99
-   :  Num_99                                                                  // 1 or 2
-      ? Num_99 === 1
-        ? Word_100Wa                                                          // 1
+   :  Num_99                                                                  // 0, 1, or 2
+      ? Num_99 === 1 ? Word_100Wa                                             // 1
         : Word_100Wa + (IsLastEffTriplet && TextAfter === "on" ? "ا" : "ان")  // For 2 اثنا or اثنان
       :   Words999 + " " + Scale;                                             // 0
    }
